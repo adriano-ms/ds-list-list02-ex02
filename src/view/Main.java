a5404fa02021f19e655a1b27c1ab3dcad4f3fd31
adriano-ms <amaciel_sanchez@hotmail.com>
2024-04-29 22:15:38 -0300

    Initialized  lists A and B

---
+++ src/view/Main.java
@@ -0,0 +1,27 @@
+package view;
+
+import br.edu.fateczl.list.List;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		List<Integer> listA = new List<>();
+		List<Integer> listB = new List<>();
+		
+		listA.addLast(3);
+		listA.addLast(5);
+		listA.addLast(8);
+		listA.addLast(12);
+		listA.addLast(9);
+		listA.addLast(7);
+		listA.addLast(16);
+		
+		listB.addLast(9);
+		listA.addLast(6);
+		listA.addLast(2);
+		listA.addLast(3);
+		listA.addLast(7);	
+	}
+
+}
