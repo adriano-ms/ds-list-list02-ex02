18880baa3157bd22defdd7a0015bd4fa87bacd49
adriano-ms <amaciel_sanchez@hotmail.com>
2024-05-06 15:25:27 -0300

    Solution

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,6 +1,7 @@
 package view;
 
 import br.edu.fateczl.list.List;
+import util.Sort;
 
 public class Main {
 
@@ -8,6 +9,8 @@ public class Main {
 		
 		List<Integer> listA = new List<>();
 		List<Integer> listB = new List<>();
+		List<Integer> listU = new List<>();
+		List<Integer> listI = new List<>();
 		
 		listA.addLast(3);
 		listA.addLast(5);
@@ -18,10 +21,48 @@ public class Main {
 		listA.addLast(16);
 		
 		listB.addLast(9);
-		listA.addLast(6);
-		listA.addLast(2);
-		listA.addLast(3);
-		listA.addLast(7);	
+		listB.addLast(6);
+		listB.addLast(2);
+		listB.addLast(3);
+		listB.addLast(7);	
+		
+		int size = listA.size();
+		for(int i = 0; i < size; i++) 
+			listU.addLast(listA.get(i));
+		
+		size = listB.size();
+		for(int i = 0; i < size; i++) 
+			listU.addLast(listB.get(i));
+		
+		Sort.bubbleSort(listU);
+		
+		for(int i = 0; i < listU.size() - 1; i++) {
+			int aux = listU.get(i);
+			if(aux == listU.get(i+1)) {
+				listU.remove(i);
+				listI.addLast(aux);
+			}
+		}
+		
+		System.out.print("List U: ");
+		printList(listU);
+		
+		System.out.print("List I: ");
+		printList(listI);
+		
+		
+	}
+	
+	public static void printList(List<?> list) {
+		try {
+			int size = list.size();
+			for(int i = 0; i < size; i++) 
+				System.out.print(list.get(i) + " ");
+			
+			System.out.println();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
